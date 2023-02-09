987d79f93fe03bccd23f70ce0f9ab6a1e684a480
maghdiel campos <maghdielcampos@gmail.com>
2023-02-08 22:40:29 -0600

    gema roo

--- app/controllers/file_uploads_controller.rb
+++ app/controllers/file_uploads_controller.rb
@@ -3,5 +3,25 @@ class FileUploadsController < ApplicationController
   end
 
   def create
+    file = params[:file_upload][:file]
+    File.open(Rails.root.join('public', 'uploads', file.original_filename), 'wb') do |f|
+      f.write(file.read)
+    end
+
+    file = Roo::Excelx.new(Rails.root.join('public', 'uploads', file.original_filename))
+    2.upto(file.last_row) do |line|
+      comprador = file.cell(line, 'A')
+      descripcion_del_item = file.cell(line, 'B')
+      precio_del_item = file.cell(line, 'C')
+      total_de_items = file.cell(line, 'D')
+      direccion_del_vendedor = file.cell(line, 'E')
+      vendedor = file.cell(line, 'F')
+
+      Data.create!(comprador: comprador, descripcion_del_item: descripcion_del_item, precio_del_item: precio_del_item, total_de_items: total_de_items, direccion_del_vendedor: direccion_del_vendedor, vendedor: vendedor)
+    end
+
+    redirect_to root_path
   end
+
 end
+
