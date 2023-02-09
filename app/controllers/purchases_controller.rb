0ee74d49c7628f7a18c7e3cc264097251f157dee
maghdiel campos <maghdielcampos@gmail.com>
2023-02-08 23:45:23 -0600

    pruchases db

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -28,7 +28,7 @@ class PurchasesController < ApplicationController
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
-      Purchase.create!(comprador: row["Cliente"], descripcion_del_item: row["Descripción del Producto"], precio_del_item: row["Precio por pieza"], total_de_items: row["Numero de piezas"], direccion_de_vendedor: row["Diección del vendedor"], vendedor: row["Nombre del Vendedor"])
+      Purchase.create!(comprador: row["Cliente"], descripcion_del_item: row["Descripcion del Producto"], precio_del_item: row["Precio por pieza"], total_de_items: row["Numero de piezas"], direccion_de_vendedor: row["Diección del vendedor"], vendedor: row["Nombre del Vendedor"])
     end
     redirect_to root_path, notice: "purchases imported."
   end
